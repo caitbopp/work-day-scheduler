3e1c7a77215c7c530e314284460708cc8ae329be
Caitlin Bopp <boppcaitlin@gmail.com>
2021-03-20 17:00:07 -0400

    Pulled descriptions from local storage

--- script.js
+++ script.js
@@ -5,33 +5,44 @@ $(document).ready(function () {
     $("#currentDay").text(todaysDate);
     // Add button click
     $(".saveBtn").on('click', function () {
-        console.log(this);
+        //console.log(this);
         var value = $(this).siblings('.description').val();
         var time = $(this).parent().attr('id');
         // Save to local storage
-        localStorage.setItem(value, time);
+        localStorage.setItem(time, value);
     });
+// Pulling descriptions from local storage and displaying on schedule
+    $(".time-block").each(function() {
+        var timeBlock = $(this).attr('id');
+        $(this).children('.description').val(localStorage.getItem(timeBlock));
+    })
 
-    // How to pull from local storage and keep info on table?
 
     // Adding classes to hour blocks based on current hour 
     function timeTracker() {
         var currentHour = moment().hour();
 
         $(".time-block").each(function () {
-            var blockHour = parseInt($(this).attr("id").split("hour")[1]);
-            console.log(blockHour, currentHour)
+            var blockHour = parseInt($(this).attr("id").split("hour-")[1]);
+            //console.log(blockHour, currentHour)
 
             if (blockHour < currentHour) {
                 $(this).addClass("past");
+                $(this).removeClass("present");
+                $(this).removeClass("future");
             }
             else if (blockHour === currentHour) {
                 $(this).addClass("present");
+                $(this).removeClass("past");
+                $(this).removeClass("future");
             }
             else {
                 $(this).addClass("future");
+                $(this).removeClass("past");
+                $(this).removeClass("present");
             }
         })
     }
     timeTracker();
+    setInterval(timeTracker, 1000);
 });
