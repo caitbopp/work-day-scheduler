211973fc952fc71bcdf4f152b0f564b98842683e
Caitlin Bopp <boppcaitlin@gmail.com>
2021-03-19 20:45:02 -0400

    added hour blocks and created script.js file

---
+++ script.js
@@ -0,0 +1,17 @@
+$(document).ready(function (){
+
+    // Add button click
+    $('.saveBtn').on('click', function(){
+        var value = $(this).siblings('.description').val();
+        var time = $(this).parent.attr('id');
+    
+        // set your item to local storage
+    })
+    
+    // Function for bringing in moment and appending to page (date at top of page?)
+    
+    // Set the classes of the elements based on the time using conditional statement - use setAttribute and set styling attribute by comparing stuff
+        // ex: if moment is past of listed col, --> each (in jquery) ex each of these if time < current time then set css class/attribute
+        
+    
+    })
